231c5a8d222e7baaa4cf8d4eb24fd2c810976b4d
LxMO <lmorris757@outlook.com>
2019-01-18 19:18:26 -0800

    complete

---
+++ server.js
@@ -0,0 +1,95 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+
+
+
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+app.get("/scrape", (req, res) => {
+    axios.get("https://thehill.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("article h2").each(function (i, element) {
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+
+        res.send("<h1>Scrape of 'The Hill' is Complete!</h1><a class='btn btn-primary btn-lg' href='/' role='button'>Now, go back to see what you've got!</a>");
+    });
+});
+
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("Notes")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", function (req, res) {
+
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findByIdAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
